7a5aeb0d9dcb297a5f9e6730d32c415efcf8e549
Toby Bro <thetobiasbrodersen@gmail.com>
2018-02-08 13:04:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == " "
-    return true
+    return false
   end
 
-  return false
+  return true
 end
